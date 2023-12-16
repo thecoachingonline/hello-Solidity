3e14e81d6abf69f4a56c0ef1c11ca9a11967d65c
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-16 01:39:32 +0000

    4096 Bits

--- hello.sol
+++ hello.sol
@@ -1,7 +1,78 @@
 pragma solidity ^0.8.0;
 
-contract HelloWorld {
-    function hello() public pure returns (string memory) {
-        return "Hello World";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract LatticeBasedCryptography {
+
+    using ECDSA for bytes32;
+
+    struct Ciphertext {
+        uint256 a;
+        uint256 b;
     }
+
+    struct PublicKey {
+        uint256 x;
+        uint256 y;
+    }
+
+    PublicKey publicKey;
+
+    constructor(uint256 x, uint256 y) {
+        publicKey = PublicKey(x, y);
+    }
+
+    function encrypt(string memory message) public returns (Ciphertext memory ciphertext) {
+        // Convert the message to a lattice point
+        uint256[] memory latticePoint = message.toLatticePoint(4096);
+
+        // Generate the noise
+        uint256[] memory noise = new uint256[](latticePoint.length);
+        for (uint256 i = 0; i < noise.length; i++) {
+            noise[i] = uint256(keccak256(abi.encodePacked(i, block.timestamp, block.number)));
+        }
+
+        // Generate the ciphertext
+        ciphertext = Ciphertext(
+            latticePoint[0] + noise[0],
+            latticePoint[1] + noise[1],
+        );
+
+        return ciphertext;
+    }
+
+    function decrypt(Ciphertext memory ciphertext) public returns (string memory message) {
+        // Convert the ciphertext to a lattice point
+        uint256[] memory latticePoint = ciphertext.toLatticePoint();
+
+        // Solve the lattice problem
+        uint256[] memory solution = latticePoint.solve();
+
+        // Convert the solution to a message
+        message = solution[0].toString();
+
+        return message;
+    }
+
+contract App {
+
+    LatticeBasedCryptography latticeBasedCryptography;
+
+    constructor(uint256 x, uint256 y) {
+        latticeBasedCryptography = new LatticeBasedCryptography(x, y);
+    }
+
+    function encryptMessage() public returns (string memory encryptedMessage) {
+        string memory message = "สวัสดีชาวโลก";
+        encryptedMessage = latticeBasedCryptography.encrypt(message);
+        return encryptedMessage;
+    }
+
+    function decryptMessage() public returns (string memory decryptedMessage) {
+        Ciphertext memory ciphertext = latticeBasedCryptography.encrypt("สวัสดีชาวโลก");
+        decryptedMessage = latticeBasedCryptography.decrypt(ciphertext);
+        return decryptedMessage;
+    }
+}
+
 }
